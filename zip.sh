7ebb51197f10cd72109c4e75fdf60986c53ce833
Niklas Keller <me@kelunik.com>
2014-11-11 09:42:03 +0100

    Change ZIP Structure

--- zip.sh
+++ zip.sh
@@ -3,5 +3,10 @@
 # if script is called outside of current folder
 cd $(dirname $(readlink -f "$0"))
 
-cd src
-zip -r ../bin/latest.zip *
+rm -r bin
+mkdir -p bin/theme/FreshBlue
+
+cp -r src/gnome-shell/* bin/theme/FreshBlue/
+cd bin/theme
+
+zip -r ../freshblue-theme.zip *
